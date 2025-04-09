64ccf46d6d57bae9f29a442e1202e7d75aa1af9d
snehov <snehov@gmail.com>
2025-04-09 09:50:43 +0200

    split quiz component to quiz and question-form

---
+++ frontend/src/pages/quiz-question-form.tsx
@@ -0,0 +1,55 @@
+import { type QuizQuestion, type AnswerIdxs, isAnsweredCorrectly } from 'model/quiz-question'
+import { QuestionForm } from './question-take'
+import { useState } from 'react'
+import type { QuizScore } from './quiz-score'
+import { ProgressBar } from './quiz/progress-bar'
+import { EvaluateButton, NextButton, BackButton } from './quiz/buttons'
+
+interface QuizQuestionProps {
+    readonly onEvaluate: (quizScore: QuizScore) => void
+    readonly quiz: QuizQuestion[]
+    readonly isEndFeedbackQuiz: boolean
+}
+
+type QuizState = readonly AnswerIdxs[]
+
+export const QuizQuestionForm = (props: QuizQuestionProps) => {
+    const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0)
+    const currentQuestion = props.quiz[currentQuestionIdx]
+    const isLastQuestion = currentQuestionIdx === props.quiz.length - 1
+
+    const [quizState, setQuizState] = useState<QuizState>([])
+    const isAnswered = quizState[currentQuestionIdx] !== undefined
+
+    const onSubmitted = (selectedAnswerIdxs: AnswerIdxs) => {
+        const newQuizState = Array.from(quizState)
+        newQuizState[currentQuestionIdx] = selectedAnswerIdxs
+        setQuizState(newQuizState)
+    }
+
+    const onNext = () => setCurrentQuestionIdx(prev => prev + 1)
+    const onEvaluate = () =>
+        props.onEvaluate({
+            correct: props.quiz.filter((question, idx) => isAnsweredCorrectly(quizState[idx], question.correctAnswers))
+                .length,
+            total: props.quiz.length,
+            answers: props.quiz.map((question, idx) => ({
+                ...question,
+                userAnswers: quizState[idx],
+                feedback: isAnsweredCorrectly(quizState[idx], question.correctAnswers) ? 'Correct' : 'Incorrect',
+            })),
+        })
+
+    const handlePreviousClick = () => setCurrentQuestionIdx(prev => prev - 1)
+
+    return (
+        <div>
+            <h2>Quiz</h2>
+            <ProgressBar current={currentQuestionIdx + 1} total={props.quiz.length} />
+            <QuestionForm key={currentQuestion.id} question={currentQuestion} onSubmitted={onSubmitted} />
+            {currentQuestionIdx > 0 && <BackButton onClick={handlePreviousClick} />}
+            {isAnswered &&
+                (!isLastQuestion ? <NextButton onClick={onNext} /> : <EvaluateButton onClick={onEvaluate} />)}
+        </div>
+    )
+}
--- frontend/src/pages/quiz.tsx
+++ frontend/src/pages/quiz.tsx
@@ -1,59 +1,8 @@
-import { type QuizQuestion, type AnswerIdxs, isAnsweredCorrectly } from 'model/quiz-question'
-import { QuestionForm } from './question-take'
+import type { QuizQuestion } from 'model/quiz-question'
 import { useState } from 'react'
 import { QuizScore } from './quiz-score'
-import { ProgressBar } from './quiz/progress-bar'
-import { EvaluateButton, NextButton, BackButton } from './quiz/buttons'
 import { useParams, useLocation } from 'react-router-dom'
-
-interface QuizQuestionProps {
-    readonly onEvaluate: (quizScore: QuizScore) => void
-    readonly quiz: QuizQuestion[]
-    readonly isEndFeedbackQuiz: boolean
-}
-
-type QuizState = readonly AnswerIdxs[]
-
-export const QuizQuestionForm = (props: QuizQuestionProps) => {
-    const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0)
-    const currentQuestion = props.quiz[currentQuestionIdx]
-    const isLastQuestion = currentQuestionIdx === props.quiz.length - 1
-
-    const [quizState, setQuizState] = useState<QuizState>([])
-    const isAnswered = quizState[currentQuestionIdx] !== undefined
-
-    const onSubmitted = (selectedAnswerIdxs: AnswerIdxs) => {
-        const newQuizState = Array.from(quizState)
-        newQuizState[currentQuestionIdx] = selectedAnswerIdxs
-        setQuizState(newQuizState)
-    }
-
-    const onNext = () => setCurrentQuestionIdx(prev => prev + 1)
-    const onEvaluate = () =>
-        props.onEvaluate({
-            correct: props.quiz.filter((question, idx) => isAnsweredCorrectly(quizState[idx], question.correctAnswers))
-                .length,
-            total: props.quiz.length,
-            answers: props.quiz.map((question, idx) => ({
-                ...question,
-                userAnswers: quizState[idx],
-                feedback: isAnsweredCorrectly(quizState[idx], question.correctAnswers) ? 'Correct' : 'Incorrect',
-            })),
-        })
-
-    const handlePreviousClick = () => setCurrentQuestionIdx(prev => prev - 1)
-
-    return (
-        <div>
-            <h2>Quiz</h2>
-            <ProgressBar current={currentQuestionIdx + 1} total={props.quiz.length} />
-            <QuestionForm key={currentQuestion.id} question={currentQuestion} onSubmitted={onSubmitted} />
-            {currentQuestionIdx > 0 && <BackButton onClick={handlePreviousClick} />}
-            {isAnswered &&
-                (!isLastQuestion ? <NextButton onClick={onNext} /> : <EvaluateButton onClick={onEvaluate} />)}
-        </div>
-    )
-}
+import { QuizQuestionForm } from './quiz-question-form'
 
 const quizQuestion1: QuizQuestion = {
     id: 1,
