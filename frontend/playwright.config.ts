1b88ec5ac9a5e9645dac52acdf7b8750ef1b8fe6
Matej Simek <matej.simek@nice.com>
2025-04-09 14:02:16 +0200

    playwright: set timeout to 15s

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -4,7 +4,7 @@ import { defineBddConfig } from 'playwright-bdd'
 const port = process.env.FE_PORT || '8080'
 
 export default defineConfig({
-    timeout: 5000,
+    timeout: 15000,
     projects: [
         { name: 'chromium', use: { browserName: 'chromium', baseURL: `http://localhost:${port}` } },
     ],
